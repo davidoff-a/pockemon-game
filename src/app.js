7a4ce948adc5403c1b5dc367543dda5c271f0548
Davydau Aliaksei <davidoff.aleksei@gmail.com>
2022-03-08 18:23:19 +0300

    feat: add useContext hook

--- src/app.js
+++ src/app.js
@@ -1,40 +1,46 @@
 import { useRouteMatch, Route, Switch, Redirect } from "react-router-dom";
-import cn from 'classnames';
+import cn from "classnames";
+import { useState } from "react";
 
 import GamePage from "./routes/game/gamePage";
 import HomePage from "./routes/home/homePage";
 import MenuHeader from "./components/MenuHeader/MenuHeader";
 import Footer from "./components/footer";
-
-import s from './style.module.css';
-
-
+import AboutPage from "./routes/about/AboutPage";
+import { TestContext } from "./context/testContext";
+import s from "./style.module.css";
 
 const App = () => {
+  const [theme, setTheme] = useState('light');
+  const handleChangeTheme = (val) => {
+    setTheme(val);
+  }
   const match = useRouteMatch("/");
+
   return (
-    <Switch>
-      <Route path='/404' render={() => <h1>404 Page Not Found</h1>} />
-      <Route>
-        <>
-          <MenuHeader bgActive={!match.isExact} />
-          <div className={cn(s.wrap, { [s.isHomePage]: match.isExact })}>
-            <Switch>
-              <Route path='/' exact component={HomePage} />
-              {/* <Route path='/home' exact component={HomePage} /> */}
-              <Route path='/game' component={GamePage} />
-              <Route path='/about' render={() => <h1>This is page About</h1>} />
-              <Route
-                path='/contacts'
-                render={() => <h1>This is page Contacts</h1>}
-              />
-              <Route render={() => <Redirect to='/404'/>} />
-            </Switch>
-          </div>
-          <Footer />
-        </>
-      </Route>
-    </Switch>
+    <TestContext.Provider value={{ theme, onChangeTheme: handleChangeTheme }}>
+      <Switch>
+        <Route path='/404' render={() => <h1>404 Page Not Found</h1>} />
+        <Route>
+          <>
+            <MenuHeader bgActive={!match.isExact} />
+            <div className={cn(s.wrap, { [s.isHomePage]: match.isExact })}>
+              <Switch>
+                <Route path='/' exact component={HomePage} />
+                <Route path='/game' component={GamePage} />
+                <Route path='/about' component={AboutPage} />
+                <Route
+                  path='/contacts'
+                  render={() => <h1>This is page Contacts</h1>}
+                />
+                <Route render={() => <Redirect to='/404' />} />
+              </Switch>
+            </div>
+            <Footer />
+          </>
+        </Route>
+      </Switch>
+    </TestContext.Provider>
   );
 };
 
---
+++ src/routes/about/AboutPage.js
@@ -0,0 +1,20 @@
+import { useContext } from "react";
+import { TestContext } from "../../context/testContext";
+
+const AboutPage = () => {
+  const themeContext = useContext(TestContext);
+  console.log("####: themeContext", themeContext);
+  const handleClick = () => {
+    themeContext.onChangeTheme(
+      themeContext.theme === "light" ? "dark" : "light"
+    );
+  };
+  return (
+    <div>
+      <h1>This is About Page</h1>
+      <button onClick={handleClick}>Change Theme</button>
+    </div>
+  );
+};
+
+export default AboutPage;
